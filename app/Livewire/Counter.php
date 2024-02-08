b9ef6951ebebf8d9eda6e1e5e54e68a4fe2afa00
Lucas Lins <lucas.fsl.92@gmail.com>
2024-02-08 00:01:23 -0300

    feature: adding first compenent livewire

---
+++ app/Livewire/Counter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public int $counter = 0;
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+
+    public function count(): void
+    {
+        $this->counter++;
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div>
+        {{$counter}}
+    </div>
+    <x-primary-button wire:click="count">
+        +1
+    </x-primary-button>
+</div>
