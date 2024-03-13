cefc6e79ea0109b71130fbc5019165ec42a42d39
Lucas Lins <lucas.fsl.92@gmail.com>
2024-03-12 21:06:21 -0300

    feature: my firsts components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,21 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="mx-auto space-y-1 max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{-- {{ __("You're logged in!") }} --}}
                     <livewire:counter>
                 </div>
+                
+            </div>
+            
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{-- {{ __("You're logged in!") }} --}}
+                    <livewire:counter-inline>
+                </div>
+                
             </div>
         </div>
     </div>
